bd1fd280163168632023c25018247f9aa3bdcfa3
Rohit Raj <rohit786483@gmail.com>
2022-07-19 16:25:17 +0530

    Initial commit

---
+++ src/Prac2.java
@@ -0,0 +1,8 @@
+public class Prac2 {
+
+    public static void main(String args[]){
+
+        // TODO: System.out.println("Text to be printed");
+        System.out.println("my name is rohit raj");
+    }
+}
